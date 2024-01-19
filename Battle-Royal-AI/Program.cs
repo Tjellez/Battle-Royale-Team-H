a3db92512b854270fde5ac954e8630d547ffcdb9
Per-Frode Pedersen <pfp@tuck.as>
2024-01-19 23:06:12 +0100

    Add static file support

--- Battle-Royal-AI/Program.cs
+++ Battle-Royal-AI/Program.cs
@@ -1,9 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMvc();
 
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapControllers();
+app.UseStaticFiles();
 
 app.Run();
