4a3bbc556988ca7d32bf3136919916a0e358627c
Magne Kjellesvik <magne.kjellesvik@gmail.com>
2024-01-20 00:43:12 +0100

    removed button logic, and changed to submit

--- Battle-Royal-AI/Program.cs
+++ Battle-Royal-AI/Program.cs
@@ -13,11 +13,3 @@ app.MapControllers();
 app.UseStaticFiles();
 
 app.Run();
-
-
-// Take in prompt from frontend
-var storyPrompt = "Can you tell me a story about a dragon and an elf?";
-var response = ChatGPT.GetGoodNightStory(storyPrompt);
-var prompt = response.Value.Choices[0].Message.Content.Substring(0, 100);
-var imageUri = await DALLe.GetDALLeImage(prompt);
-// Send image uri and story text to frontend
